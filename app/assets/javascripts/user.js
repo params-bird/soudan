ce0c5e3e66d8fb0d9b4a1ef7bccf4a98c609e8c7
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-07 14:48:58 +0900

    Update view_page

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -59,9 +59,14 @@ $(function(){
   });
 });
 
-// resizeイベントの取得
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
 window.addEventListener('resize', () => {
-  // あとは上記と同じスクリプトを実行
+  // We execute the same script as before
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
