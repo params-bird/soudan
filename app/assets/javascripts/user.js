86ea78467362b9f6753283dbd29ceeaa39eaa450
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-07 14:42:34 +0900

    Update view_page

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -59,4 +59,9 @@ $(function(){
   });
 });
 
-
+// resizeイベントの取得
+window.addEventListener('resize', () => {
+  // あとは上記と同じスクリプトを実行
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
