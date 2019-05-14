84c21181d5605a21f3099f9d1bb7dc74b8c0045d
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-14 22:25:29 +0900

    update

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,14 +5,6 @@
   #画像が回転してしまう問題を解消
   process :fix_rotate
 
-  def fix_rotate
-      manipulate! do |img|
-          img = img.auto_orient
-          img = yield(img) if block_given?
-          img
-      end
-  end
-
   if Rails.env.development?
     storage :file
   elsif Rails.env.test?
@@ -21,18 +13,17 @@
     storage :fog
   end
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
+  def fix_rotate
+      manipulate! do |img|
+          img = img.auto_orient
+          img = yield(img) if block_given?
+          img
+      end
+  end
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
 
   # 画像の上限を700pxにする
   process :resize_to_limit => [400, 400]
@@ -40,27 +31,11 @@
   # 保存形式をJPGにする
   process :convert => 'png'
 
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   # アップロードできる拡張子を制限
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
 
-  # Override the filename of the uploaded files:
-  # # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
     "#{secure_token}.#{file.extension}" if original_filename.present?
   end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,11 +8,11 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3用の設定
       provider: 'AWS',
-      region: ENV['S3_REGION'],
-      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_S3_REGION'],
+      aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_IAM_SECRET_ACCESS_KEY'],
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_directory     =  ENV['AWS_S3_BUCKET']
     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   end
 
