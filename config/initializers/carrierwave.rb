3ee615d8cff6e98f9021055df33d08b303f3d8c4
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-14 15:54:32 +0900

    update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,34 +1,14 @@
-# require 'carrierwave/storage/abstract'
-# require 'carrierwave/storage/file'
-# require 'carrierwave/storage/fog'
-
-# CarrierWave.configure do |config|
-#   if Rails.env.production?
-#     config.storage :fog
-#     config.fog_provider = 'fog/aws'
-#     config.fog_directory  = 'soudan-sns'
-#     config.fog_credentials = {
-#       provider: 'AWS',
-#       aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
-#       aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-#       region: ENV['S3_REGION'],
-#       path_style: true
-#     }
-#   else
-#     config.storage :file
-#     config.enable_processing = false if Rails.env.test?
-#   end
-# end
-
-# CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_REGION'],
+    aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+    region: ENV['S3_REGION'],
   }
   config.fog_directory = 'soudan-app'
 end
