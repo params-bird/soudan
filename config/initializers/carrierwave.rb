24bb4452c16a2cc2874bd320d640ac16322f8ffe
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-14 12:18:08 +0900

    update_s3_setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,12 +6,11 @@ CarrierWave.configure do |config|
   if Rails.env.development? || Rails.env.test?
     config.storage = :file
   elsif Rails.env.production?
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
+    CarrierWave.configure do |config|
+      config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      aws_access_key_id: ENV['ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1'
     }
     config.fog_directory  = 'soudan-sns'
