9b279180e240bac3657cb958619078e922bc9fa6
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-14 18:24:08 +0900

    update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,31 +1,21 @@
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-    region: 'us-east-2',
-  }
-    config.fog_directory  = 'soudan-sns'
-    config.fog_public     = false
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
-  end
-
-
 require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-    region: 'us-east-2',
-  }
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      # Amazon S3用の設定
+      provider: 'AWS',
+      region: ENV['S3_REGION'],
+      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+  end
 
-  config.fog_directory  = 'soudan-sns'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/soudan-sns'
+  # 日本語ファイル名の設定
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 end
