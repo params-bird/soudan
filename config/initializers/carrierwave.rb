a2f00bcd57be3d8f0fe04602e29f5d671f43f520
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-14 16:54:49 +0900

    update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,20 +1,12 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-    region: ENV['S3_REGION'],
+    region: 'us-east-2,
   }
-  case Rails.env
-  when 'production'
-      config.fog_directory  = 'soudan-app'
-      config.asset_host = 'https://s3.amazonaws.com/soudan-app'
+    config.fog_directory  = 'soudan-app'
+    config.fog_public     = false
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
   end
-end
-
-CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
