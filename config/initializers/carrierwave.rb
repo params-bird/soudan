5d69b05a6853ee5113503b3590613aca18a6381b
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-14 13:06:00 +0900

    update_s3_setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,7 +16,7 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1'
     }
     config.fog_directory  = 'soudan-sns'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/soudan-sns'
+    config.asset_host = 'https://s3.amazonaws.com/soudan-sns'
     end
   end
 end
