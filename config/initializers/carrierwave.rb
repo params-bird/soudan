b9be90aa400f623037a987fde3392aa56ace1261
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-14 17:54:30 +0900

    update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,3 +10,22 @@ CarrierWave.configure do |config|
     config.fog_public     = false
     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
   end
+
+
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+    region: 'us-east-2',
+  }
+
+  config.fog_directory  = 'soudan-sns'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/soudan-sns'
+end
