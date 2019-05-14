95d2a97ae0bc39281d4243a92e27935517536146
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-14 12:27:20 +0900

    update_s3_setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,16 +7,16 @@ set :default_env, {
 
 
 # secret.yaml（が必要の際に）デプロイする際の設定
-# set :linked_files, %w{ config/secrets.yml }
-# desc 'upload secrets.yml'
-# task :upload do
-#   on roles(:app) do |host|
-#     if test "[ ! -d #{shared_path}/config ]"
-#       execute "mkdir -p #{shared_path}/config"
-#     end
-#     upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
-#   end
-# end
-# before :starting, 'deploy:upload'
-# after :finishing, 'deploy:cleanup'
-# end
+set :linked_files, %w{ config/secrets.yml }
+desc 'upload secrets.yml'
+task :upload do
+  on roles(:app) do |host|
+    if test "[ ! -d #{shared_path}/config ]"
+      execute "mkdir -p #{shared_path}/config"
+    end
+    upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+  end
+end
+before :starting, 'deploy:upload'
+after :finishing, 'deploy:cleanup'
+end
