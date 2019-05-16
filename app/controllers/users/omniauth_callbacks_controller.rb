9fc906593273ab179b2ee85211e4adb58d418cfd
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-17 01:31:01 +0900

    update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,29 +4,58 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     basic_action
   end
 
-  private
-  def basic_action
-    @omniauth = request.env['omniauth.auth']
-    if @omniauth.present?
-      @profile = User.where(provider: @omniauth['provider'], uid: @omniauth['uid']).first
-      if @profile
-        @profile.set_values(@omniauth)
-        sign_in(:user, @profile)
+
+
+  def callback_for(provider)
+    oauth = request.env["omniauth.auth"]
+
+    if User.where(email: oauth[:info][:email]).blank?
+      @user = User.create_oauth(request.env["omniauth.auth"])
+      if @user.persisted?
+        bypass_sign_in(@user)
+        redirect_to new_profile_path
+        set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
       else
-        @profile = User.new(provider: @omniauth['provider'], uid: @omniauth['uid'], name: @omniauth['info']['displayName'], avater: @omniauth['info']['pictureUrl'])
-        email = @omniauth['info']['email'] ? @omniauth['info']['email'] : "#{@omniauth['uid']}-#{@omniauth['provider']}@example.com"
-        @profile = current_user || User.create!(provider: @omniauth['provider'], uid: @omniauth['uid'], email: email, name: @omniauth['info']['name'], avater: @omniauth['info']['pictureUrl'], password: Devise.friendly_token[0, 20])
-        @profile.set_values(@omniauth)
-        sign_in(:user, @profile)
-        redirect_to edit_user_path(@profile.user.id) and return
+        session["devise.#{provider}_data"] = oauth.except("extra")
+        redirect_to registration_select_path
       end
+    else
+      snscredential = SnsCredential.find_sns(oauth)
+      @user = SnsCredential.check_sns(snscredential, oauth)
+      bypass_sign_in(@user)
+      redirect_to root_path, notice: 'ログインしました'
     end
-    flash[:notice] = "ログインしました"
+  end
+
+  def failure
     redirect_to root_path
   end
 
-  def fake_email(uid,provider)
-      return "#{auth.uid}-#{auth.provider}@example.com"
-  end
+
+
+  # private
+  # def basic_action
+  #   @omniauth = request.env['omniauth.auth']
+  #   if @omniauth.present?
+  #     @profile = User.where(provider: @omniauth['provider'], uid: @omniauth['uid']).first
+  #     if @profile
+  #       @profile.set_values(@omniauth)
+  #       sign_in(:user, @profile)
+  #     else
+  #       @profile = User.new(provider: @omniauth['provider'], uid: @omniauth['uid'])
+  #       email = @omniauth['info']['email'] ? @omniauth['info']['email'] : "#{@omniauth['uid']}-#{@omniauth['provider']}@example.com"
+  #       @profile = current_user || User.create!(provider: @omniauth['provider'], uid: @omniauth['uid'], email: email, name: @omniauth['info']['name'], avater: @omniauth['info']['pictureUrl'], password: Devise.friendly_token[0, 20])
+  #       @profile.set_values(@omniauth)
+  #       sign_in(:user, @profile)
+  #       redirect_to edit_user_path(@profile.user.id) and return
+  #     end
+  #   end
+  #   flash[:notice] = "ログインしました"
+  #   redirect_to root_path
+  # end
+
+  # def fake_email(uid,provider)
+  #     return "#{auth.uid}-#{auth.provider}@example.com"
+  # end
 
 end
