ea69650cb1f7550378e30f9db2613ade4e8b2b20
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-17 01:43:36 +0900

    update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,58 +4,29 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     basic_action
   end
 
-
-
-  def callback_for(provider)
-    oauth = request.env["omniauth.auth"]
-
-    if User.where(email: oauth[:info][:email]).blank?
-      @user = User.create_oauth(request.env["omniauth.auth"])
-      if @user.persisted?
-        bypass_sign_in(@user)
-        redirect_to new_profile_path
-        set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+  private
+  def basic_action
+    @omniauth = request.env['omniauth.auth']
+    if @omniauth.present?
+      @profile = User.where(provider: @omniauth['provider'], uid: @omniauth['uid']).first
+      if @profile
+        @profile.set_values(@omniauth)
+        bypass_sign_in(:user, @profile)
       else
-        session["devise.#{provider}_data"] = oauth.except("extra")
-        redirect_to registration_select_path
+        @profile = User.new(provider: @omniauth['provider'], uid: @omniauth['uid'])
+        email = @omniauth['info']['email'] ? @omniauth['info']['email'] : "#{@omniauth['uid']}-#{@omniauth['provider']}@example.com"
+        @profile = current_user || User.create!(provider: @omniauth['provider'], uid: @omniauth['uid'], email: email, name: @omniauth['info']['name'], avater: @omniauth['info']['image'], password: Devise.friendly_token[0, 20])
+        @profile.set_values(@omniauth)
+        bypass_sign_in(:user, @profile)
+        redirect_to edit_user_path(@profile.user.id) and return
       end
-    else
-      snscredential = SnsCredential.find_sns(oauth)
-      @user = SnsCredential.check_sns(snscredential, oauth)
-      bypass_sign_in(@user)
-      redirect_to root_path, notice: 'ログインしました'
     end
-  end
-
-  def failure
+    flash[:notice] = "ログインしました"
     redirect_to root_path
   end
 
-
-
-  # private
-  # def basic_action
-  #   @omniauth = request.env['omniauth.auth']
-  #   if @omniauth.present?
-  #     @profile = User.where(provider: @omniauth['provider'], uid: @omniauth['uid']).first
-  #     if @profile
-  #       @profile.set_values(@omniauth)
-  #       sign_in(:user, @profile)
-  #     else
-  #       @profile = User.new(provider: @omniauth['provider'], uid: @omniauth['uid'])
-  #       email = @omniauth['info']['email'] ? @omniauth['info']['email'] : "#{@omniauth['uid']}-#{@omniauth['provider']}@example.com"
-  #       @profile = current_user || User.create!(provider: @omniauth['provider'], uid: @omniauth['uid'], email: email, name: @omniauth['info']['name'], avater: @omniauth['info']['pictureUrl'], password: Devise.friendly_token[0, 20])
-  #       @profile.set_values(@omniauth)
-  #       sign_in(:user, @profile)
-  #       redirect_to edit_user_path(@profile.user.id) and return
-  #     end
-  #   end
-  #   flash[:notice] = "ログインしました"
-  #   redirect_to root_path
-  # end
-
-  # def fake_email(uid,provider)
-  #     return "#{auth.uid}-#{auth.provider}@example.com"
-  # end
+  def fake_email(uid,provider)
+      return "#{auth.uid}-#{auth.provider}@example.com"
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,42 +19,23 @@ class User < ApplicationRecord
 
   mount_uploader :avater, ImageUploader
 
+  def set_values(omniauth)
+    return if provider.to_s != omniauth['provider'].to_s || uid != omniauth['uid']
+    credentials = omniauth['credentials']
+    info = omniauth['info']
 
-  def self.create_oauth(auth)
-    user = User.create(
-      provider: auth.provider,
-      uid: auth.uid,
-      name: auth.info.name,
-      avater: auth.info.image,
-      email:    auth.info.email,
-      password: Devise.friendly_token[0, 20]
-      )
-    return user
+    self.access_token = credentials['token']
+    self.access_secret = credentials['secret']
+    self.credentials = credentials.to_json
+    self.name = info.name
+    self.image = info.image
+
+    self.set_values_by_raw_info(omniauth['extra']['raw_info'])
   end
 
-
-
-
-
-
-
-  # def set_values(omniauth)
-  #   return if provider.to_s != omniauth['provider'].to_s || uid != omniauth['uid']
-  #   credentials = omniauth['credentials']
-  #   info = omniauth['info']
-
-  #   self.access_token = credentials['refresh_token']
-  #   self.access_secret = credentials['secret']
-  #   self.credentials = credentials.to_json
-  #   self.name = info['name']
-  #   self.avater = info['pictureUrl']
-
-  #   self.set_values_by_raw_info(omniauth['extra']['raw_info'])
-  # end
-
-  # def set_values_by_raw_info(raw_info)
-  #   self.raw_info = raw_info.to_json
-  #   self.save!
-  # end
+  def set_values_by_raw_info(raw_info)
+    self.raw_info = raw_info.to_json
+    self.save!
+  end
 
 end
