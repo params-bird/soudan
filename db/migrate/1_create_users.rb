a38fed671868b324104d4151340a66c41d79e2a4
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-16 22:02:02 +0900

    update

--- db/migrate/1_create_users.rb
+++ db/migrate/1_create_users.rb
@@ -2,6 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users, id: false do |t|
       t.string :id, limit: 36, null: false, primary_key: true
+      t.string :uid
+      t.string :provider
       t.string :name, null: false
       t.binary :avater
       t.timestamps
