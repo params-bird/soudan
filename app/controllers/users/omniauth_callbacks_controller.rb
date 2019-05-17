45a221337038f02803a4a8b27f15f308876b4f9f
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-17 16:36:28 +0900

    update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -34,11 +34,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       if @user
         bypass_sign_in(@user)
       else
-        image_url = @auth.info.image
+        remote_image_url = @auth.info.image
         @user = User.create(
           name:     @auth.info.name,
           email:    @auth.info.email,
-          avater:   image_url,
+          avater:   remote_image_url,
           provider: @auth.provider,
           uid:      @auth.uid,
           password: Devise.friendly_token[0, 20],
