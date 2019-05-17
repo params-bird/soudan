e75c9b677ee81055ce7509cbe515173f5a42d7a7
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-17 23:16:35 +0900

    update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -27,17 +27,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 
   def google_oauth2
-    @auth = request.env['omniauth.auth']
-    if @auth.present?
-      @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
-      if @user
-        bypass_sign_in(@user)
-        redirect_to user_mypage_path(@user.id) and return
-      end
-    else
-      redirect_to new_user_registration_path and return
-    end
-  end
+    @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
+
+     if @user.persisted?
+       flash[:notice] = I18n.t "devise.omniauth_callbacks.success",
+                               :kind => "Google"
+       sign_in_and_redirect @user, :event => :authentication
+     else
+       session["devise.google_data"] = request.env["omniauth.auth"]
+       redirect_to new_user_registration_url
+     end
+   end
 
   private
   def oauth_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,18 +19,16 @@ class User < ApplicationRecord
   has_many :thanks, dependent: :delete_all
   has_many :bads, dependent: :delete_all
 
-  def self.find_for_google_oauth2
-    @user = User.where(email: auth.info.email).first
+  def self.find_for_google_oauth2(auth)
+    user = User.where(email: auth.info.email).first
     unless user
-      @user = User.create(name:     auth.info.name,
+      user = User.create(name:     auth.info.name,
                          provider: auth.provider,
                          uid:      auth.uid,
                          email:    auth.info.email,
-                         remote_image_url: auth.info.image,
-                         password: Devise.friendly_token[0, 20]
-                         )
+                         password: Devise.friendly_token[0, 20])
     end
-    @user
+    user
   end
 
 end
