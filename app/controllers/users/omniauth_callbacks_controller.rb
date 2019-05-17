8adbb5b6744e6d93660c35385c6a737ba806eec6
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-17 15:56:35 +0900

    update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,15 +2,23 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 
   def line
-    @omniauth = request.env['omniauth.auth']
-    if @omniauth.present?
-      @user = User.where(provider: @omniauth['provider'], uid: @omniauth['uid']).first
+    @auth = request.env['omniauth.auth']
+    if @auth.present?
+      @user = User.where(provider: @auth['provider'], uid: @auth['uid']).first
       if @user
         bypass_sign_in(@user)
       else
-        @user = User.new(provider: @omniauth['provider'], uid: @omniauth['uid'])
-        email = @omniauth['info']['email'] ? @omniauth['info']['email'] : "#{@omniauth['uid']}-#{@omniauth['provider']}@example.com"
-        @user = current_user || User.create!(provider: @omniauth['provider'], uid: @omniauth['uid'], email: email, name: @omniauth['info']['name'], avater: @omniauth['info']['image'], password: Devise.friendly_token[0, 20])
+        @user = User.new(provider: @auth['provider'], uid: @auth['uid'])
+        email = @auth['info']['email'] ? @auth['info']['email'] : "#{@auth['uid']}-#{@auth['provider']}@example.com"
+        @user = current_user || User.create!(
+          provider: @auth['provider'],
+          uid: @auth['uid'],
+          image: ＠auth.info.image,
+          email: email,
+          name: @auth['info']['name'],
+          avater: @auth['info']['image'],
+          password: Devise.friendly_token[0, 20]
+        )
         bypass_sign_in(@user)
         redirect_to user_mypage_path(@user.id) and return
       end
@@ -29,6 +37,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         @user = User.create(
           name:     @auth.info.name,
           email:    @auth.info.email,
+          image:    ＠auth.info.image,
           provider: @auth.provider,
           uid:      @auth.uid,
           password: Devise.friendly_token[0, 20],
