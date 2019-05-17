787cd7c52e6d2ba4ace4723b85f6703885e63d94
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-17 14:05:29 +0900

    update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,22 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  def line
-    callback_for(:line)
+
+  def google
+    @user = User.find_for_google(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+      sign_in_and_redirect @user, event: :authentication
+      session[:user_id] = @user.id #add
+    else
+      session['devise.google_data'] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
+    end
   end
 
-  def google_oauth2
-    callback_for(:google)
+
+  def line
+    callback_for(:line)
   end
 
   private
@@ -25,6 +36,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
     redirect_to user_mypage_path(@user.id) and return
   end
+
 
   def fake_email(uid,provider)
       return "#{auth.uid}-#{auth.provider}@example.com"
