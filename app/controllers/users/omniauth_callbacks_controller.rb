2660f16f3ba2e3310f58bf19a1bb0ee030aa662d
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-17 15:36:50 +0900

    update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,31 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-
-  def google_oauth2
-    callback_for(:google)
-  end
-
-  def callback_for(provider)
-    @auth = request.env["omniauth.auth"]
-    if @auth.present?
-      @user = User.find_by(email: @auth.info.email)
-      if @user
-        bypass_sign_in(@user)
-      else
-        user = User.create(
-          name:     auth.info.name,
-          email:    auth.info.email,
-          provider: auth.provider,
-          uid:      auth.uid,
-          password: Devise.friendly_token[0, 20],
-        )
-      end
-    else
-      redirect_to new_user_registration_path and return
-    end
-  end
-
-
   def line
     basic_action
   end
@@ -40,12 +14,49 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       else
         @user = User.new(provider: @omniauth['provider'], uid: @omniauth['uid'])
         email = @omniauth['info']['email'] ? @omniauth['info']['email'] : "#{@omniauth['uid']}-#{@omniauth['provider']}@example.com"
-        @user = current_user || User.create!(provider: @omniauth['provider'], uid: @omniauth['uid'], name: @omniauth['info']['name'], avater: @omniauth['info']['image'], password: Devise.friendly_token[0, 20])
+        @user = current_user || User.create!(provider: @omniauth['provider'], uid: @omniauth['uid'], email: email, name: @omniauth['info']['name'], avater: @omniauth['info']['image'], password: Devise.friendly_token[0, 20])
+        # @user.set_values(@omniauth)
         bypass_sign_in(@user)
         redirect_to user_mypage_path(@user.id) and return
       end
     end
     redirect_to new_user_registration_path and return
   end
+
+
+  def google_oauth2
+    callback_for(:google)
+  end
+
+  def callback_for(provider)
+    @auth = request.env["omniauth.auth"]
+    if @auth.present?
+      @user = User.find_by(email: @auth.info.email)
+      if @user
+        bypass_sign_in(@user)
+      else
+        user = User.create(
+          name:     @auth.info.name,
+          email:    @auth.info.email,
+          provider: @auth.provider,
+          uid:      @auth.uid,
+          password: Devise.friendly_token[0, 20],
+        )
+      end
+    else
+      redirect_to new_user_registration_path and return
+    end
+  end
+
+
+
+
+
+
+
+
+
+
+
 
 end
