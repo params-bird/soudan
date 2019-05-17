e039eaaa2738da5e7f23b1ca7e864f2c86ec04f1
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-17 17:08:44 +0900

    update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,7 +8,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       if @user
         bypass_sign_in(@user)
       else
-        @user = User.new(provider: @auth['provider'], uid: @auth['uid'])
+        @user = User.new(oauth_params)
+        @user.remote_avater_url = @auth.info.image
         email = @auth['info']['email'] ? @auth['info']['email'] : "#{@auth['uid']}-#{@auth['provider']}@example.com"
         @user = current_user || User.create!(
           provider: @auth['provider'],
@@ -16,7 +17,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
           image:    ＠auth.info.image,
           email:     email,
           name:     @auth['info']['name'],
-          avater:   remote_image_url,
+          avater:   @user.remote_avater_url,
           password: Devise.friendly_token[0, 20],
           @user.remote_avater_url = ＠auth['info']['image']
         )
@@ -27,7 +28,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     redirect_to new_user_registration_path and return
   end
 
-
   def google_oauth2
     @auth = request.env["omniauth.auth"]
     if @auth.present?
@@ -35,14 +35,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       if @user
         bypass_sign_in(@user)
       else
+        @user = User.new(oauth_params)
+        @user.remote_avater_url = @auth.info.image
         @user = User.create(
           name:     @auth.info.name,
           email:    @auth.info.email,
-          avater:   remote_image_url,
+          avater:   @user.remote_avater_url,
           provider: @auth.provider,
           uid:      @auth.uid,
           password: Devise.friendly_token[0, 20],
-          @user.remote_avater_url = @auth.info.image
         )
         bypass_sign_in(@user)
         redirect_to user_mypage_path(@user.id) and return
@@ -52,5 +53,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
+  private
+  def oauth_params
+    params.require(:user).permit(:provider, :uid, :image, :email, :name, :avater, :password)
+  end
 
 end
