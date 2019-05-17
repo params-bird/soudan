7751d5ba44acdc45a368963df378a2142301b19e
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-17 17:10:54 +0900

    update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,12 +14,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         @user = current_user || User.create!(
           provider: @auth['provider'],
           uid:      @auth['uid'],
-          image:    ＠auth.info.image,
-          email:     email,
           name:     @auth['info']['name'],
+          email:     email,
           avater:   @user.remote_avater_url,
           password: Devise.friendly_token[0, 20],
-          @user.remote_avater_url = ＠auth['info']['image']
         )
         bypass_sign_in(@user)
         redirect_to user_mypage_path(@user.id) and return
@@ -38,11 +36,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         @user = User.new(oauth_params)
         @user.remote_avater_url = @auth.info.image
         @user = User.create(
+          provider: @auth.provider,
+          uid:      @auth.uid,
           name:     @auth.info.name,
           email:    @auth.info.email,
           avater:   @user.remote_avater_url,
-          provider: @auth.provider,
-          uid:      @auth.uid,
           password: Devise.friendly_token[0, 20],
         )
         bypass_sign_in(@user)
