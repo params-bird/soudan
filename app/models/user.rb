b29850146ca6d98ff744366c673bf4e435f6c2cb
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-17 14:08:04 +0900

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,21 +20,20 @@ class User < ApplicationRecord
   has_many :bads, dependent: :delete_all
 
 
-
   protected
   def self.find_for_google(auth)
     user = User.find_by(email: auth.info.email)
-
     unless user
-      user = User.create(name:     auth.info.name,
-                         email: auth.info.email,
-                         provider: auth.provider,
-                         uid:      auth.uid,
-                         avater:   auth.info.image,
-                         token:    auth.credentials.token,
-                         password: Devise.friendly_token[0, 20],
+      user = User.create(
+        name:     auth.info.name,
+        email: auth.info.email,
+        provider: auth.provider,
+        uid:      auth.uid,
+        avater:   auth.info.image,
+        token:    auth.credentials.token,
+        password: Devise.friendly_token[0, 20],
+      )
     end
     user
   end
-
 end
