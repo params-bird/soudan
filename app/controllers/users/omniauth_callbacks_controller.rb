068d8a2c36568d3951ebd8319bb9f8b0a3493df4
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-17 15:51:17 +0900

    update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,7 +11,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         @user = User.new(provider: @omniauth['provider'], uid: @omniauth['uid'])
         email = @omniauth['info']['email'] ? @omniauth['info']['email'] : "#{@omniauth['uid']}-#{@omniauth['provider']}@example.com"
         @user = current_user || User.create!(provider: @omniauth['provider'], uid: @omniauth['uid'], email: email, name: @omniauth['info']['name'], avater: @omniauth['info']['image'], password: Devise.friendly_token[0, 20])
-        # @user.set_values(@omniauth)
         bypass_sign_in(@user)
         redirect_to user_mypage_path(@user.id) and return
       end
@@ -23,7 +22,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     @auth = request.env["omniauth.auth"]
     if @auth.present?
-      @user = User.where(provider: @omniauth['provider'], uid: @omniauth['uid']).first
+      @user = User.find_by(email: @auth.info.email)
       if @user
         bypass_sign_in(@user)
       else
@@ -41,16 +40,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_path and return
     end
   end
-
-
-
-
-
-
-
-
-
-
 
 
 end
