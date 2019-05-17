522fe25b83ace008ee4ec6ec7aae28b68cadaf23
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-18 00:18:42 +0900

    update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -25,23 +25,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
 
-
   def google_oauth2
-    @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
-
-     if @user.persisted?
-       flash[:notice] = I18n.t "devise.omniauth_callbacks.success",
-                               :kind => "Google"
-       sign_in_and_redirect @user, :event => :authentication
-     else
-       session["devise.google_data"] = request.env["omniauth.auth"]
-       redirect_to new_user_registration_url
-     end
-   end
-
-  private
-  def oauth_params
-    params.require(:user).permit(:provider, :uid, :image, :email, :name, :image, :password)
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    if @user.persisted?
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+      bypass_sign_in(@user)
+      redirect_to user_mypage_path(@user.id)
+    else
+      session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,6 @@ class User < ApplicationRecord
          :omniauthable,
          omniauth_providers: %i[line google_oauth2]
 
-
   has_many :messages
   has_many :topics
   has_many :chat_rooms
@@ -19,17 +18,35 @@ class User < ApplicationRecord
   has_many :thanks, dependent: :delete_all
   has_many :bads, dependent: :delete_all
 
-  def self.find_for_google_oauth2(auth)
-    user = User.where(email: auth.info.email).first
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
     unless user
-      user = User.create(name:     auth.info.name,
-                         provider: auth.provider,
-                         uid:      auth.uid,
-                         email:    auth.info.email,
-                         remote_image_url: @auth.info.image,
-                         password: Devise.friendly_token[0, 20])
+        user = User.create(
+          name: data['name'],
+          email: data['email'],
+          provider: data['provider'],
+          uid:      data['uid'],
+          remote_image_url: data['info.image'],
+          password: Devise.friendly_token[0,20]
+        )
     end
     user
-  end
+end
+
+
+  # def self.find_for_google_oauth2(auth)
+  #   user = User.where(email: auth.info.email).first
+  #   unless user
+  #     user = User.create(name:     auth.info.name,
+  #                        provider: auth.provider,
+  #                        uid:      auth.uid,
+  #                        email:    auth.info.email,
+  #                        remote_image_url: @auth.info.image,
+  #                        password: Devise.friendly_token[0, 20])
+  #   end
+  #   user
+  # end
 
 end
