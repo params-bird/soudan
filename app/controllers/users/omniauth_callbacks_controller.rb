1d0ebc08e80f6373aa19bd2b5bb72c22d94a1070
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-19 11:10:31 +0900

    update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -38,22 +38,22 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 
   def google_oauth2
-    auth = request.env['omniauth.auth']
+    @auth = request.env['omniauth.auth']
     # auth = User.from_omniauth(request.env['omniauth.auth'])
-    ＠user = User.where(provider: auth[:provider], email: auth[:email]).first
+    ＠user = User.where(provider: @auth[:provider], email: @auth[:email]).first
     if ＠user
       # 既にサービスに登録していればログイン処理
       bypass_sign_in(@user)
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
       redirect_to user_mypage_path(@user.id) and return
     else
-      ＠user = User.create(
-        name: auth['info']['name'],
-        email: auth['info']['email'],
-        provider: auth['provider'],
-        uid:      auth['uid'],
-        remote_image_url: auth['info']['image'],
-        password: Devise.friendly_token[0,20]
+      @user = current_user || User.create!(
+        provider: @auth[:provider],
+        uid:      @auth[:uid],
+        remote_image_url: @auth[:info][:image],
+        name:     @auth[:info][:name],
+        email:    @auth[:email],
+        password: Devise.friendly_token[0, 20],
       )
       bypass_sign_in(@user)
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
