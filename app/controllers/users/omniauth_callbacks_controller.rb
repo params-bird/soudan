72bfb23506c67196debf934e353bb413821dfdda
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-19 10:54:41 +0900

    update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -59,11 +59,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
       # ログインさせマイページに遷移
       redirect_to user_mypage_path(@user.id) and return
-      else
-        # 失敗の際はアカウン登録画面に遷移
-        session['devise.line_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow   some session stores
-        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-      end
     end
   end
 end
