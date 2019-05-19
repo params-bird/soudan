9ef1b1217750a1943b540b6656b2af3608e714f9
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-19 10:08:44 +0900

    update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -36,15 +36,26 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 
   def google_oauth2
+    # @user = User.from_omniauth(request.env['omniauth.auth'])
+    # if @user.persisted?
+    #   flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+    #   bypass_sign_in(@user)
+    #   redirect_to user_mypage_path(@user.id)
+    # else
+    #   session['devise.google_data'] = request.env['omniauth.auth']
+    #   redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    # end
+
+
     @user = User.from_omniauth(request.env['omniauth.auth'])
     if @user.persisted?
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-      bypass_sign_in(@user)
-      redirect_to user_mypage_path(@user.id)
+      sign_in_and_redirect @user, event: :authentication
     else
-      session['devise.google_data'] = request.env['omniauth.auth']
+      session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
+
   end
 
 end
