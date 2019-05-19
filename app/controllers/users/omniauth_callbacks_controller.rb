78340d85d98610dc1e3841b8de6aa41da8606f77
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-19 10:41:56 +0900

    update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -40,33 +40,31 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     auth = request.env['omniauth.auth']
     # auth = User.from_omniauth(request.env['omniauth.auth'])
-    ＠user = User.where(email: auth['email']).first
+    ＠user = User.where(provider: auth[:provider], email: auth['email']).first
     if ＠user
       # 既にサービスに登録していればログイン処理
       bypass_sign_in(@user)
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
       redirect_to user_mypage_path(@user.id) and return
     else
-      unless user
-        ＠user = User.create(
-          name: auth['info']['name'],
-          email: auth['info']['email'],
-          provider: auth['provider'],
-          uid:      auth['uid'],
-          remote_image_url: auth['info']['image'],
-          password: Devise.friendly_token[0,20]
-        )
-        if @user.persisted?
-          # 新たにレコードが作られたか
-          flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-          bypass_sign_in(@user)
-          # ログインさせマイページに遷移
-          redirect_to user_mypage_path(@user.id) and return
-        else
-          # 失敗の際はアカウン登録画面に遷移
-          session['devise.line_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow   some session stores
-          redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-        end
+      ＠user = User.create(
+        name: auth['info']['name'],
+        email: auth['info']['email'],
+        provider: auth['provider'],
+        uid:      auth['uid'],
+        remote_image_url: auth['info']['image'],
+        password: Devise.friendly_token[0,20]
+      )
+      if @user.persisted?
+        # 新たにレコードが作られたか
+        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+        bypass_sign_in(@user)
+        # ログインさせマイページに遷移
+        redirect_to user_mypage_path(@user.id) and return
+      else
+        # 失敗の際はアカウン登録画面に遷移
+        session['devise.line_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow   some session stores
+        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
       end
     end
   end
