24b75286422b4833a9c0f2c64ec930305a87d20a
params.bird@gmail.com <taira.kenichi@gmail.com>
2019-05-19 09:27:18 +0900

    update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -36,7 +36,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 
   def google_oauth2
-    if User.where(email: oauth[:info][:email]).blank?
     @user = User.from_omniauth(request.env['omniauth.auth'])
     if @user.persisted?
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
